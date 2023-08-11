6e2d853bfb94e9c12c20c9be3604927ab6249d5d
amans <amansarosh@gmail.com>
2023-08-11 15:14:22 -0700

    Navbar relocated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,35 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* Header */}
+
+        <header className="bg-slate-200">
+          <nav className="flex py-5 justify-center" aria-label="Global">
+            <div className="flex gap-x-12">
+              <Link
+                href="/search"
+                className="text-sm font-semibold leading-6 text-gray-900"
+              >
+                Start Here
+              </Link>
+              <Link
+                href="/"
+                className="text-xl font-bold leading-6 text-gray-900 overline"
+              >
+                ALPSE
+              </Link>
+              <Link
+                href="/about"
+                className="text-sm font-semibold leading-6 text-gray-900"
+              >
+                About Us
+              </Link>
+            </div>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
