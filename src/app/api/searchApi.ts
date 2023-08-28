57897b6252341af6130394103765e705977bb053
amans <amansarosh@gmail.com>
2023-08-28 07:09:02 -0700

    API Logic

---
+++ src/app/api/searchApi.ts
@@ -0,0 +1,18 @@
+"use server";
+
+export async function searchFlight(e: FormData): Promise<Object> {
+  
+  const flight_number = e.get("flight_number") as string;
+
+  const res = await fetch(
+    `${process.env.API_BASE}flight?flight_icao=${flight_number}&api_key=${process.env.API_KEY}`,
+    {
+      cache: "no-cache",
+    }
+  );
+  const data = await res.json();
+  console.log(data);
+  
+  return data.response;
+
+}
