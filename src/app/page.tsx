2e1e773440472e6162b9b06f89d4586d1447c827
amans <amansarosh@gmail.com>
2023-09-01 12:36:52 -0700

    Cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
             <div className="mt-5 flex items-center justify-center gap-x-6 lg:justify-start text-slate-200">
               <Link
                 href="/search"
-                className="rounded-md bg-slate-200 px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+                className="rounded-md bg-slate-200 px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100"
               >
                 Get started
               </Link>
